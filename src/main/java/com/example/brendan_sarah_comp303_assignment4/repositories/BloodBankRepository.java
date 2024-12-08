52b048f4601745fd9028a668160325c927a95a97
sarahshields77 <sshield9@my.centennialcollege.ca>
2024-12-08 17:15:41 -0500

    added submission info comments in files

--- src/main/java/com/example/brendan_sarah_comp303_assignment4/repositories/BloodBankRepository.java
+++ src/main/java/com/example/brendan_sarah_comp303_assignment4/repositories/BloodBankRepository.java
@@ -1,3 +1,10 @@
+/*
+ * Assign 4 Group:
+ * Student 1: Brendan James 301257167
+ * Student 2: Sarah Shields 301264350
+ * Submission Date: December 9th, 2024
+ */
+
 package com.example.brendan_sarah_comp303_assignment4.repositories;
 
 import com.example.brendan_sarah_comp303_assignment4.entities.BloodBank;
